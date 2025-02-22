7db1be68b4cc6b3f87b747f695f7e1d9acbfc4c8
Valeria Chavira <ChaviraVa@outlook.com>
2025-02-22 15:24:34 -0700

    Agregue botones a Menu

--- Menu.Designer.cs
+++ Menu.Designer.cs
@@ -32,6 +32,8 @@
             this.btnRegistroProducto = new System.Windows.Forms.Button();
             this.btnRegistroPersonal = new System.Windows.Forms.Button();
             this.btnPruebas = new System.Windows.Forms.Button();
+            this.btnProductos = new System.Windows.Forms.Button();
+            this.btnPersonal = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label3
@@ -102,12 +104,50 @@
             this.btnPruebas.UseWaitCursor = true;
             this.btnPruebas.Click += new System.EventHandler(this.btnPruebas_Click);
             // 
+            // btnProductos
+            // 
+            this.btnProductos.BackColor = System.Drawing.Color.MintCream;
+            this.btnProductos.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.btnProductos.FlatAppearance.BorderSize = 2;
+            this.btnProductos.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnProductos.Font = new System.Drawing.Font("Nirmala Text", 10.2F, System.Drawing.FontStyle.Bold);
+            this.btnProductos.ForeColor = System.Drawing.Color.DarkCyan;
+            this.btnProductos.Location = new System.Drawing.Point(128, 170);
+            this.btnProductos.Margin = new System.Windows.Forms.Padding(4);
+            this.btnProductos.Name = "btnProductos";
+            this.btnProductos.Size = new System.Drawing.Size(169, 68);
+            this.btnProductos.TabIndex = 10;
+            this.btnProductos.Text = "Productos disponibles";
+            this.btnProductos.UseMnemonic = false;
+            this.btnProductos.UseVisualStyleBackColor = false;
+            this.btnProductos.UseWaitCursor = true;
+            // 
+            // btnPersonal
+            // 
+            this.btnPersonal.BackColor = System.Drawing.Color.Honeydew;
+            this.btnPersonal.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.btnPersonal.FlatAppearance.BorderSize = 2;
+            this.btnPersonal.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnPersonal.Font = new System.Drawing.Font("Nirmala Text", 10.2F, System.Drawing.FontStyle.Bold);
+            this.btnPersonal.ForeColor = System.Drawing.Color.SeaGreen;
+            this.btnPersonal.Location = new System.Drawing.Point(325, 170);
+            this.btnPersonal.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPersonal.Name = "btnPersonal";
+            this.btnPersonal.Size = new System.Drawing.Size(169, 68);
+            this.btnPersonal.TabIndex = 11;
+            this.btnPersonal.Text = "Personal Actual";
+            this.btnPersonal.UseMnemonic = false;
+            this.btnPersonal.UseVisualStyleBackColor = false;
+            this.btnPersonal.UseWaitCursor = true;
+            // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Info;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPersonal);
+            this.Controls.Add(this.btnProductos);
             this.Controls.Add(this.btnPruebas);
             this.Controls.Add(this.btnRegistroPersonal);
             this.Controls.Add(this.btnRegistroProducto);
@@ -125,5 +165,7 @@
         private System.Windows.Forms.Button btnRegistroProducto;
         private System.Windows.Forms.Button btnRegistroPersonal;
         private System.Windows.Forms.Button btnPruebas;
+        private System.Windows.Forms.Button btnProductos;
+        private System.Windows.Forms.Button btnPersonal;
     }
 }
