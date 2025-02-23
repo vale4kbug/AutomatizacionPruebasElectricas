087a8e1efac1a3dc939b18bbd020d219ba1ee869
Valeria Chavira <ChaviraVa@outlook.com>
2025-02-23 16:00:27 -0700

    Agregando elementos a RegistroPersonal

--- RegistroPersonal.cs
+++ RegistroPersonal.cs
@@ -23,5 +23,50 @@ namespace AutomatizacionPruebasElectricas
             this.Hide();
             menuforma.Show();
         }
+
+        private void btnRegistrarPersonal_Click(object sender, EventArgs e)
+        {
+            if (true)
+            {
+                MessageBox.Show("Usuario Registrado." +
+                  "Usuario registrado correctamente.", "Usuario Registrado", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("Error: No es posible registrar el usuario." +
+                    "Checa que todos los campos esten llenos.", "Error XoX", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnEliminarPersonal_Click(object sender, EventArgs e)
+        {
+            if (true)
+            {
+
+            }
+           else if (true)
+            {
+                MessageBox.Show("Error: No es posible eliminar el usuario." +
+                 "El usuario no existe. Revisa los datos", "Error XoX", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("Error: No es posible eliminar el usuario." +
+                   "Revisa que todos los campos esten llenos.", "Error XoX", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnModificarPersonal_Click(object sender, EventArgs e)
+        {
+            if (true)
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Error: No es posible modificar registrar el usuario." +
+                "El usuario no existe. Revisa los datos", "Error XoX", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
