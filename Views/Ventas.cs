97a3e4d3e683ee5eedfadec04b78b0848382462b
roman-zamorano <romanzamorano08@gmail.com>
2025-03-05 16:53:47 -0700

    se añadio la forma de ventas

---
+++ Views/Ventas.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutomatizacionPruebasElectricas.Views
+{
+    public partial class Ventas : Form
+    {
+        public Ventas()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegistrarVentas_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEliminarVentas_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnModificarVentas_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
